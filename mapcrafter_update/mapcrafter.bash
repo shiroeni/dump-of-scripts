5f980daa9f94498ee9306d280aef29a6136441c9
shiroeni <shiroeni@outlook.com>
2020-01-03 00:25:02 +0400

    added script for mapcrafter

---
+++ mapcrafter_update/mapcrafter.bash
@@ -0,0 +1,18 @@
+#!/usr/bin/bash
+# Generates and update map of server
+
+if [ -d "/home/minecraft/output" ]; then
+  echo "Drop old renders";
+  rm -rf /home/minecraft/output; true
+fi
+
+echo "Render new map";
+/usr/local/bin/mapcrafter -c render.conf -j 2
+
+# TODO: use symlinks in mapcrafter
+echo "Remove old render"
+rm -rf /var/www/html/*
+
+echo "Copy new render"
+cp -r /home/minecraft/output /var/www/html
+
